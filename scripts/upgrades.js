52e859fd9f7f8ab311326d85ce29e6e8df094047
rohanmishra <104006850+rohanmishr@users.noreply.github.com>
2023-01-09 12:13:58 -0500

    Create upgrades.js

---
+++ scripts/upgrades.js
@@ -0,0 +1,75 @@
+function buyUpgrade(upgrade){
+    //clicker ranks
+    if(upgrade=="bronzeClicker" && weight >= 100){
+      weight -= 100;
+      baseWeightGain = 5;
+      clickerRank = 2;
+      $("#bronzeClicker").css("display","none");
+    }else if(upgrade == "silverClicker" && weight >= 1000){
+      weight -= 1000;
+      baseWeightGain = 25;
+      clickerRank = 3;
+      $("#silverClicker").css("display","none");
+    }else if(upgrade == "goldClicker" && weight >= 10000){
+      weight -= 10000;
+      baseWeightGain = 125;
+      clickerRank = 4;
+      $("#goldClicker").css("display","none");
+    }else if(upgrade == "rubyClicker" && weight >= 100000){
+      weight -= 100000;
+      baseWeightGain = 625;
+      clickerRank = 5;
+      $("#rubyClicker").css("display","none");
+    }else if(upgrade == "sapphireClicker" && weight >= 1000000){
+      weight -= 1000000;
+      baseWeightGain = 3125;
+      clickerRank = 6;
+      $("#sapphireClicker").css("display","none");
+    }else if(upgrade == "emeraldClicker" && weight >= 10000000){
+      weight -= 10000000;
+      baseWeightGain = 15625;
+      clickerRank = 7;
+      $("#emeraldClicker").css("display","none");
+    }else if(upgrade == "diamondClicker" && weight >= 100000000){
+        weight -= 100000000;
+        baseWeightGain = 78125;
+        clickerRank = 8;
+        $("#diamondClicker").css("display","none");
+    }else if(upgrade == "amethystClicker" && weight >= 1000000000){
+        weight -= 1000000000;
+        baseWeightGain = 390625;
+        clickerRank = 9;
+        $("#amethystClicker").css("display","none");
+    }else if(upgrade == "platinumClicker" && weight >= decodeNum('10B')){
+        weight -= decodeNum('10B');
+        baseWeightGain = 1953125;
+        clickerRank = 10;
+        $("#platinumClicker").css("display","none");
+
+    //overall upgrades
+    }else if(upgrade == "packagedChips" && weight >= 200){
+      weight -= 200;
+      weightMultiplier *= 1.5;
+      $("#packagedChips").css("display","none");
+    }else if(upgrade == "burgersAndFries" && weight >= 500){
+      weight -= 500;
+      weightMultiplier *= 1.5;
+      $("#burgersAndFries").css("display","none");
+    }else if(upgrade == "xlMilkshake" && weight >= 1000){
+      weight -= 1000;
+      weightMultiplier *= 1.5;
+      $("#xlMilkshake").css("display","none");
+    }else if(upgrade == "doubleStuffedOreos" && weight >= 2500){
+      weight -= 2500;
+      weightMultiplier *= 1.5;
+      $("#doubleStuffedOreos").css("display","none");
+    }else if(upgrade == "weightGainingProgram" && weight >= 5000){
+      weight -= 5000;
+      weightMultiplier *= 1.5;
+      $("#weightGainingProgram").css("display","none");
+    }else if(upgrade == "discordServer" && weight >= 12000){
+        weight -= 12000;
+        weightMultiplier *= 1.5;
+        $("#discordServer").css("display","none");
+    }
+  }
