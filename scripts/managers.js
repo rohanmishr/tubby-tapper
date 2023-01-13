e5029250e6db25cc506c60a5f29a143515bf1604
rohanmishra <104006850+rohanmishr@users.noreply.github.com>
2023-01-13 18:20:56 -0500

    migrate

--- scripts/managers.js
+++ scripts/managers.js
@@ -1,31 +1,39 @@
 function manager(rawName, name, price, boost, amount){
-    this.rawName = rawName;
-    this.name = name;
-    this.price = price;
-    this.boost = boost;
-    this.amount = amount;
+  this.rawName = rawName;
+  this.name = name;
+  this.price = price;
+  this.boost = boost;
+  this.amount = amount;
 }
 
 function managerPurchaseHandler(m){
-    weight -= m.price;
-    m.amount += 1;
-    m.price *= 1.1;
-    var htmlSelector = "#"+m.rawName+"-text";
-    $(htmlSelector).text(m.name+" ("+abbvNum(m.price)+")");
+  weight -= m.price;
+  m.amount += 1;
+  m.price *= 1.1;
+  var htmlSelector = "#"+m.rawName+"-text";
+  $(htmlSelector).text(m.name+" ("+abbvNum(m.price)+")");
 
 }
 
 //list of managers
-var chefs = new manager("chefs", "Chef", 250, 5, 0);
-var badHealthAdvisors = new manager("badHealthAdvisors", "Bad Health Advisors", 1500, 25, 0);
-var discordMods = new manager("discordMods", "Discord Mods", 5000, 100, 0);
+var chefs = new manager("chefs", "Chef", 250, 50, 0);
+var badHealthAdvisors = new manager("badHealthAdvisors", "Bad Health Advisors", 1500, 250, 0);
+var discordMods = new manager("discordMods", "Discord Mods", 1000, 500, 0);
+var testThing = new manager("testThing", "Test Thing", 1, 1000, 0);
 
 function buyManager(manager){
-    if(manager=="chefs" && weight >= chefs.price){
-      managerPurchaseHandler(chefs)
-    }else if(manager=="badHealthAdvisors" && weight >= badHealthAdvisors.price){
-      managerPurchaseHandler(badHealthAdvisors)
-    }else if(manager="discordMods" && weight >= discordMods.price){
-      managerPurchaseHandler(discordMods);
-    }
+  //buying chefs
+  if(manager=="chefs" && weight >= chefs.price){
+    managerPurchaseHandler(chefs)
+  //buying bad health advisors
+  }else if(manager=="badHealthAdvisors" && weight >= badHealthAdvisors.price){
+    managerPurchaseHandler(badHealthAdvisors)
+  //buying discord mods
+  }else if(manager="discordMods" && weight >= discordMods.price){
+    managerPurchaseHandler(discordMods)
+ // buying test thing
+  }else if(manager="testThing" && weight >= testThing.price){
+    managerPurchaseHandler(testThing)
+  }
+
 }
