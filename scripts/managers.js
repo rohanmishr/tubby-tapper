a69ee4f4027fdcaa4a6c55e4a3df5e221172f944
rohanmishra <104006850+rohanmishr@users.noreply.github.com>
2023-01-19 16:24:43 -0500

    Update managers.js

--- scripts/managers.js
+++ scripts/managers.js
@@ -12,7 +12,6 @@ function managerPurchaseHandler(m){
   m.price *= 1.1;
   var htmlSelector = "#"+m.rawName+"-text";
   $(htmlSelector).text(m.name+" ("+abbvNum(m.price)+")");
-
 }
 
 //list of managers
@@ -20,19 +19,23 @@ var chefs = new manager("chefs", "Chef", 250, 50, 0);
 var badHealthAdvisors = new manager("badHealthAdvisors", "Bad Health Advisors", 1500, 250, 0);
 var discordMods = new manager("discordMods", "Discord Mods", 5000, 550, 0);
 var fatSurgeons = new manager("fatSurgeons", "Fat Surgeons", 25000, 1300, 0);
+var gamers = new manager("gamers", "Gamers", 100000, 3700, 0);
 
 function buyManager(manager){
   //buying chefs
   if(manager=="chefs" && weight >= chefs.price){
-    managerPurchaseHandler(chefs)
+    managerPurchaseHandler(chefs);
   //buying bad health advisors
   }else if(manager=="badHealthAdvisors" && weight >= badHealthAdvisors.price){
-    managerPurchaseHandler(badHealthAdvisors)
+    managerPurchaseHandler(badHealthAdvisors);
   //buying discord mods
-  }else if(manager="discordMods" && weight >= discordMods.price){
-    managerPurchaseHandler(discordMods)
+  }else if(manager=="discordMods" && weight >= discordMods.price){
+    managerPurchaseHandler(discordMods);
  // buying fat surgeons
-  }else if(manager="fatSurgeons" && weight >= fatSurgeons.price){
-    managerPurchaseHandler(fatSurgeons)
+  }else if(manager=="fatSurgeons" && weight >= fatSurgeons.price){
+    managerPurchaseHandler(fatSurgeons);
+  //buying gamers
+  }else if(manager=="gamers" && weight >= gamers.price){
+    managerPurchaseHandler(gamers);
   }
 }
